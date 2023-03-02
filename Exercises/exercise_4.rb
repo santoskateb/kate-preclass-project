568799aaf9bd87bcc02b4db7f9071134ac7c89ac
santoskateb <santoskateb02@gmail.com>
2023-03-02 15:07:17 +0800

    exercise 4: Hash

---
+++ Exercises/exercise_4.rb
@@ -0,0 +1,16 @@
+# Exercise4
+# Make a Hash of names with value age.
+# Write function that takes an age and returns the name base on the given age.
+# Print on console the result.
+
+def find_name(age_value)
+  names = { kate: 23, dy: 32, raquel: 26, joe: 17 }
+
+  names.each do |name,age|
+    if age == age_value
+      return name
+    end
+  end
+end
+
+print find_name(23)
