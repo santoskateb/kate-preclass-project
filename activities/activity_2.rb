4780dd105d2da7a8375e4747fd6e99cb7d6faf0a
santoskateb <santoskateb02@gmail.com>
2023-03-02 16:07:19 +0800

    activity 2: fullname

--- activities/activity_2.rb
+++ activities/activity_2.rb
@@ -3,3 +3,12 @@
 #  Values will be your respective names.
 #  Write a function that takes a Hash and returns the full name.
 #  Print on console the result.
+
+def fullname(first_middle_last)
+
+  my_name = first_middle_last[:first_name ] + first_middle_last[:middle_name ] + first_middle_last[:last_name]
+  return my_name
+end
+
+my_name = { first_name: 'Kathleen', middle_name: 'B', last_name: 'Santos' }
+p fullname(my_name)
