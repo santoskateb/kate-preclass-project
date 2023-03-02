39de38015c9d5b1c4dfcb3be3ff4a1fdfe2c95e6
santoskateb <santoskateb02@gmail.com>
2023-03-02 16:11:36 +0800

    activity 2: fullname

--- activities/activity_2.rb
+++ activities/activity_2.rb
@@ -5,10 +5,9 @@
 #  Print on console the result.
 
 def fullname(first_middle_last)
-
-  my_name = first_middle_last[:first_name ] + first_middle_last[:middle_name ] + first_middle_last[:last_name]
+  my_name = first_middle_last[:first_name ] +' '+ first_middle_last[:middle_name ]+' '+ first_middle_last[:last_name]
   return my_name
 end
 
-my_name = { first_name: 'Kathleen ', middle_name: 'B ', last_name: 'Santos' }
+my_name = { first_name: 'Kathleen', middle_name: 'B', last_name: 'Santos' }
 p fullname(my_name)
